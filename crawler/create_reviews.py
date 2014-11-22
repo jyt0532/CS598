c2001c25249d914e5422913efcb20134a4888471
Bo-Yu, Chiang <jyt0532@gmail.com>
2014-11-22 15:30:54 -0600

    fix the bug , combined all reviews in a single restaurant

--- crawler/create_reviews.py
+++ crawler/create_reviews.py
@@ -10,18 +10,20 @@ import random
 import json
 import time
 import math
-def reviews_in_page_url(page_url, name):
-    reviews_arr = []
-    dic = {}
-    dic["name"] = name
+def append_reviews_from_page_url(restaurant_reviews, page_url, name):
+    if not restaurant_reviews:
+        restaurant_reviews["name"] = name
+        reviews_arr = []
+    else:
+        reviews_arr = restaurant_reviews["reviews"]
+
     soup = BeautifulSoup(urllib2.urlopen(page_url).read())
     #print soup.prettify()
     page_reviews = soup.findAll("p", {"itemprop": "description"})
     for elem in page_reviews:
 #        print elem.get_text()
         reviews_arr.append(elem.get_text())
-    dic["reviews"] = reviews_arr
-    return dic
+    restaurant_reviews["reviews"] = reviews_arr
 
 def crawl_reviews(): 
     default_url = "http://www.yelp.com"
@@ -30,9 +32,11 @@ def crawl_reviews():
     count = 0
     for elem in json_data:
         num_of_reviews = elem["review_count"]
+        restaurant_reviews = {}
         for i in range(int(math.ceil(num_of_reviews/40))):
             page_url = default_url + elem["review_url"] + "?start=" + str(i*40)
-            total_reviews.append(reviews_in_page_url(page_url, elem["name"]))
+            append_reviews_from_page_url(restaurant_reviews, page_url, elem["name"])
+        total_reviews.append(restaurant_reviews)
         count = count + 1
         print count
     with open('reviews.json', 'w') as outfile:

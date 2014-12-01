864d552ea31ae0d26f73b438455a469b610f5800
Bo-Yu, Chiang <jyt0532@gmail.com>
2014-12-01 16:54:37 -0600

    Started render ranking

--- js/cs598.js
+++ js/cs598.js
@@ -3,64 +3,29 @@ var open_code_flag = 1;
 function _init() {
     $('#search-result').hide();
     show_aspects();
-    button_click_action();
+    search_button_click_action();
     quota_control();
     get_catogories();
 
-    //  $("#search_button").click(_handle_search_button_click);
-    //  $("#login").click(_handle_login_request);
-    /*  ajax_call(
-        "./post.php?request=category",
-        null,
-        function(result) {
-        var search_cat = $("#search_category");
-        search_cat.val(2);
-        var post_cat = $("#post_category");
-        for (var i=0; i<result.length; i++) {
-        var option = new_elem("option", result[i])
-        if (result[i] == "All") {
-        option.attr("selected", "selected");
-        } else {
-        post_cat.append($("<option></option>").append(result[i]));
-        }
-        search_cat.append(option);
-        }
-        },
-        function() {
-        alert("Updating categories failed");
-        }
-        );*/
+
+    $('.select2-input').delegate("input", "click", function(){
+        $('.select2-input').css("height", "29px");    
+        $('.select2-input').css("margin", "auto");    
+    })
 }
 function get_catogories(){
-    var availableTags = [
-        "ActionScript",
-        "AppleScript",
-        "Asp",
-        "BASIC",
-        "C",
-        "C++",
-        "Clojure",
-        "COBOL",
-        "ColdFusion",
-        "Erlang",
-        "Fortran",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "JavaScript",
-        "Lisp",
-        "Perl",
-        "PHP",
-        "Python",
-        "Ruby",
-        "Scala",
-        "Scheme"
-            ];
-   /* $( "#tags" ).autocomplete({
-        source: availableTags
-    });*/
-    append_tags_options(availableTags);
-    $("#tags").select2();
+    ajax_call(
+        "php/server/category.php",
+        null,
+        function(availableTags){
+            append_tags_options(availableTags);
+            $("#tags").select2();
+       },
+       function(response){
+            alert("Get category failure");
+       },
+       "get"
+    );
 }
 function append_tags_options(availableTags){
     for(var i = 0; i < availableTags.length; i++){
@@ -132,29 +97,40 @@ function quota_control(){
             $(cur_elem).parent().attr("rating", rating);
             $("#quota_num").text(get_total_used_quota());
             for(var i = 0 ; i < rating; i++){
-            $(cur_elem).removeClass('img-not-selected').addClass('img-selected');
-            var prev = cur_elem.previousSibling;
-            cur_elem = prev;
+                $(cur_elem).removeClass('img-not-selected').addClass('img-selected');
+                var prev = cur_elem.previousSibling;
+                cur_elem = prev;
             }
 
-            });
+    });
 
 }
-function button_click_action(){
+function search_button_click_action(){
     $('#search-btn').click(function(){
-            $('.intro').hide();
-            $('#search').hide();
-            $('#about').hide();
-            $('#download').hide();
-            $('#contact').hide();
-            $('#map').hide();
-            $('#search-result').show();
-            $('.input-area').clone().appendTo('.new-input-area');
-            $('#main-nav').removeClass("navbar-custom navbar-fixed-top");
-            //$('#start-searching').hide();
-            });
+        $('.intro').hide();
+        $('#search').hide();
+        $('#about').hide();
+        $('#download').hide();
+        $('#contact').hide();
+        $('#map').hide();
+        $('#search-result').show();
+        $('.result_area').clone().appendTo('.new-input-area');
+        $('.page-header').hide();
+        $('.select2-container').css('margin-left', '0px');
+        $('#s2id_tags').css('width', '200px');
+        $('#main-nav').removeClass("navbar-custom navbar-fixed-top");
 
-
+        var result = a = [{"a":1, "b":2}, {"a":3, "b":4}];
+        render_restaurant_result(result);
+    });
+}
+function render_restaurant_result(result){
+    for(var i = 0; i < result.length; i++){
+        var restaurant_div = new_elem("div","" , "result"+i);
+        restaurant_div.append(new_elem("div", result[i].a, "result"+ i + "_name"));
+        restaurant_div.append(new_elem("div", result[i].b, "result"+ i + "_star"));
+        $('.result-area').append(restaurant_div);
+    }
 }
 
 
@@ -163,12 +139,12 @@ function ajax_call(url, data, successCallback, errorCallback, type) {
         type = "get";
     }
     $.ajax({
-url: url,
-type: type,
-//dataType: 'json',
-data: data,
-success: successCallback,
-error: errorCallback
+    url: url,
+    type: type,
+    dataType: 'json',
+    data: data,
+    success: successCallback,
+    error: errorCallback
 });
 }
 
@@ -184,44 +160,6 @@ function new_elem(name, content, id) {
     }
 
     return elem.attr("id", id);
-}
-
-
-function _handle_home_tab_click() {
-    hide_all_tabs_but("home");
-    $("#search_tab").parent().removeClass("active"); 
-    $('#home_tab').parent().addClass("active"); 
-}
-function _handle_keyword_tab_click() {
-    hide_all_tabs_but("keyword");
-    $("#post_tab").parent().removeClass("active"); 
-    $("#home_tab").parent().removeClass("active"); 
-    $("#search_tab").parent().removeClass("active"); 
-    $("#keyword_tab").parent().addClass("active"); 
-}
-
-function _handle_search_tab_click() {
-    hide_all_tabs_but("search");
-    empty_search_result();
-    $("#result_detail_div").hide();
-    $("#post_tab").parent().removeClass("active"); 
-    $("#keyword_tab").parent().removeClass("active"); 
-    $("#home_tab").parent().removeClass("active"); 
-    $("#search_tab").parent().addClass("active"); 
-}
-
-function _handle_search_tab_button_click() {
-    $("#search_tab").trigger("click"); 
-}
-function _handle_learn_more_button_click() {
-    $("#keyword_tab").trigger("click"); 
-}
-
-function _handle_post_tab_click() {
-    hide_all_tabs_but("post");
-    $("#home_tab").parent().removeClass("active"); 
-    $("#search_tab").parent().removeClass("active"); 
-    $("#post_tab").parent().addClass("active"); 
 }
 
 
--- php/server/category.php
+++ php/server/category.php
@@ -3,6 +3,6 @@ include '../query/query.php';
 
 $q = new Query("../db/restaurant_index_5.json", "../db/category.json", "../db/IDMapping.json");
 
-echo $q -> getAllCategory();
+echo json_encode($q -> getAllCategory());
 
 ?>

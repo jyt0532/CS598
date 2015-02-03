3adc835fc9797515ebcfb9e1fb6b4e155a441777
jxu22 <owensy.aiesec@gmail.com>
2015-02-03 14:06:31 -0600

    san xiao

--- crawler/data/util/parseRating.py
+++ crawler/data/util/parseRating.py
@@ -6,11 +6,8 @@ f = open(path, 'r')
 dic = {}
 
 for line in f:
-  lst = line.split("\t")
-  lst = map(lambda s:s.strip(), lst)
-  dic[lst[0]] = lst[1:]
+  lst = line.split("\t\t")
+  print(lst[1])
 f.close()
 
-with open('rating.json', 'w') as output:
-  json.dump(dic, output, indent=2)
 
